6d2b1e2f50136850a78b8142d9cf149fe0bcdc94
Clayton Easterbrooks & Vaughn Dice <student@epicodus.com>
2015-08-10 09:47:05 -0700

    write specs, is triangle yes/no

---
+++ spec/triangles_spec.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('triangles')
+
+describe(Triangle) do
+  describe("#is_triangle?") do
+    it("Returns true if shape is triangle") do
+      test_triangle = Triangle.new(3, 3, 4)
+      expect((test_triangle).is_triangle?).to(eq(true))
+    end
+    it("Returns false if shape is not triangle") do
+      test_triangle = Triangle.new(3, 3, 7)
+      expect((test_triangle).is_triangle?).to(eq(false))
+  end
+end
